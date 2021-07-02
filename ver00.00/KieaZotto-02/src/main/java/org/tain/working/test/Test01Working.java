edd722bd0b3064abebbf1890f43e2e4c44baafbd
grtlinux <grtlinux@gmail.com>
2021-07-02 09:25:35 +0900

    add samples

--- ver00.00/KieaZotto-02/src/main/java/org/tain/working/test/Test01Working.java
+++ ver00.00/KieaZotto-02/src/main/java/org/tain/working/test/Test01Working.java
@@ -1,10 +1,17 @@
 package org.tain.working.test;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.FileTime;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.stereotype.Component;
 import org.tain.utils.CurrentInfo;
@@ -86,7 +93,15 @@ public class Test01Working {
 		}
 		
 		if (Boolean.TRUE) {
-			// resolve
+			// walk
+			Path path = Paths.get("/home/users/hello/world");
+			List<Path> lst = null;
+			try (Stream<Path> walk = Files.walk(path)) {
+				lst = walk
+						.filter(file -> Files.isRegularFile(file))
+						.collect(Collectors.toList());
+			}
+			lst.forEach(file -> System.out.println(file));
 		}
 		
 		if (Boolean.TRUE) {
@@ -97,6 +112,88 @@ public class Test01Working {
 			System.out.println(">>>>> path1: " + path1);  // /home/users/hello/world
 			System.out.println(">>>>> path2: " + path2);  // /home/users/hello/world/My/Name/is/Kang
 			System.out.println(">>>>> path3: " + path3);  //                         My/Name/is/Kang
+		}
+		
+		if (Boolean.TRUE) {
+			// walk filter
+			Path path = Paths.get("/home/users/hello/world");
+			if (!Files.isDirectory(path)) {
+				throw new IllegalArgumentException("Path must be a directory.");
+			}
+			
+			List<Path> lst = null;
+			try (Stream<Path> walk = Files.walk(path)) {
+				lst = walk
+						.filter(Files::isReadable)  // read permission
+						.filter(Files::isRegularFile)  // is a file
+						.filter(p -> p.getFileName().toString().endsWith(".txt"))
+						.collect(Collectors.toList());
+			}
+			lst.forEach(file -> System.out.println(file));
+		}
+		
+		if (Boolean.TRUE) {
+			// use function
+			Path path = Paths.get("/home/users/hello/world");
+			long fileSize = 1024 * 1024 * 10;  // 10MB
+			List<Path> lst = findByFileSize(path, fileSize);
+			lst.forEach(x -> System.out.println(x));
+		}
+		
+		if (Boolean.TRUE) {
+			// set all files to be modified new date
+			Path path = Paths.get("/home/users/hello/world");
+			Instant yesterday = Instant.now().minus(1, ChronoUnit.DAYS);
+			setAllFilesModifiedDate(path, yesterday);
+		}
+	}
+	
+	// collect files less then fileSize
+	private List<Path> findByFileSize(Path path, long fileSize) throws Exception {
+		if (!Files.isDirectory(path)) {
+			throw new IllegalArgumentException("Path must be a directory..");
+		}
+		
+		List<Path> lst;
+		// walk file tree, no more recursive loop
+		try (Stream<Path> walk = Files.walk(path)) {
+			lst = walk
+					.filter(Files::isReadable)  // read permission
+					.filter(p -> !Files.isDirectory(p))  // is a file
+					.filter(p -> checkFileSize(p, fileSize))
+					.collect(Collectors.toList());
+		}
+		
+		return lst;
+	}
+	
+	// check file size, if more than fileSize, then return true
+	private boolean checkFileSize(Path path, long fileSize) {
+		boolean result = false;
+		try {
+			if (Files.size(path) >= fileSize) {
+				result = true;
+			}
+		} catch (IOException e) {
+			System.err.println("Unable to get the file size of this file: " + path);
+		}
+		return result;
+	}
+	
+	// set all files' last modified time to this instant
+	private void setAllFilesModifiedDate(Path path, Instant instant) throws IOException {
+		try (Stream<Path> walk = Files.walk(path)) {
+			walk
+				.filter(Files::isReadable)   // read permission
+				.filter(Files::isRegularFile)  // file only
+				.forEach(p -> {
+					try {
+						// set last modified time
+						Files.setLastModifiedTime(p, FileTime.from(instant));
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				});
 		}
 	}
 }
